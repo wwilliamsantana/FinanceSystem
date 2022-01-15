16d82786aa104a2d041fde533dbe11274aee7d81
William <euwilliamsantana@gmail.com>
2022-01-15 10:06:13 -0300

    Add FormatCurrency / CssClass income/expense

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const Modal = {
   }
 }
 
-const transaction = [
+const transactions = [
   {
     id: 1,
     description: 'Luz',
@@ -43,15 +43,20 @@ const Transaction = {
 }
 
 const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
   addTransaction(transaction, index) {
     const tr = document.createElement('tr')
     tr.innerHTML = DOM.innerHTMLTransaction(transaction)
-    console.log(tr.innerHTML)
+    DOM.transactionsContainer.appendChild(tr)
   },
   innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
     const html = `
       <td class="description">${transaction.description}</td>
-      <td class="expense">${transaction.amount}</td>
+      <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
       <img src="./assets/minus.svg" alt="Remover Transações">
@@ -62,4 +67,19 @@ const DOM = {
   }
 }
 
-DOM.addTransaction(transaction[0])
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? '-' : ''
+    value = String(value).replace(/\D/g, '')
+    value = Number(value) / 100
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+    return signal + value
+  }
+}
+
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction)
+})
