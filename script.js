83dc3d701d13b22c8f494e86514a9fe5387343da
William <euwilliamsantana@gmail.com>
2022-01-15 14:28:04 -0300

    Remove / index, data-set

--- script.js
+++ script.js
@@ -73,9 +73,10 @@ const DOM = {
   addTransaction(transaction, index) {
     const tr = document.createElement('tr')
     tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    tr.dataset.index = index
     DOM.transactionsContainer.appendChild(tr)
   },
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
 
     const amount = Utils.formatCurrency(transaction.amount)
@@ -85,7 +86,7 @@ const DOM = {
       <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
-      <img src="./assets/minus.svg" alt="Remover Transações">
+      <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover Transações">
       </td>
     `
 
@@ -188,8 +189,8 @@ const Form = {
 
 const App = {
   init() {
-    Transaction.all.forEach(function (transaction) {
-      DOM.addTransaction(transaction)
+    Transaction.all.forEach(function (transaction, index) {
+      DOM.addTransaction(transaction, index)
     })
     DOM.updateBalance()
   },
